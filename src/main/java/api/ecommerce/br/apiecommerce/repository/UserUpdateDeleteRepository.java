521c453e2d2130721e1c133c90e9cea9a62ce24c
Caique Souza Fonseca <c.souzaf@hotmail.com>
2023-04-26 17:23:51 -0300

    Update user email

--- src/main/java/api/ecommerce/br/apiecommerce/repository/UserUpdateDeleteRepository.java
+++ src/main/java/api/ecommerce/br/apiecommerce/repository/UserUpdateDeleteRepository.java
@@ -1,15 +1,12 @@
 package api.ecommerce.br.apiecommerce.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import api.ecommerce.br.apiecommerce.model.UserModel;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserModel, Long>{
-    
-    // UserModel findByEmail(String email);
-
-    Optional<UserModel> findByEmail(String email);
+public interface UserUpdateDeleteRepository extends JpaRepository<UserModel, String > {
+     
+    UserModel findByEmail(String email);
 }
--- src/main/java/api/ecommerce/br/apiecommerce/service/UserService.java
+++ src/main/java/api/ecommerce/br/apiecommerce/service/UserService.java
@@ -3,26 +3,26 @@ package api.ecommerce.br.apiecommerce.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import api.ecommerce.br.apiecommerce.model.UserModel;
-import api.ecommerce.br.apiecommerce.repository.UserRepository;
+import api.ecommerce.br.apiecommerce.repository.UserUpdateDeleteRepository;
 
 public class UserService {
     
     @Autowired
-    private UserRepository userRepository;
+    private UserUpdateDeleteRepository userUpdateDeleteRepository;
 
     public UserModel updateEmail(String email, UserModel userModel){
         
-        UserModel userEmail = userRepository.findByEmail(email);
+         UserModel userEmail = userUpdateDeleteRepository.findByEmail(email);
 
         if(userEmail == null) {
-            throw new UsernameNotFoundException("Usuário não encontrado pelo email" + email);
+            throw new UsernameNotFoundException("Usuário não encontrado pelo email: " + email);
         }
         
         else {
             userEmail.setEmail(userModel.getEmail());
         }
 
-        return userRepository.save(userModel);
+        return userUpdateDeleteRepository.save(userEmail);
     }
 
 
