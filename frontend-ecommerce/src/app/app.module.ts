d160923a67d3480efe58f50cc28172a527a7a7e5
Caique Souza Fonseca <c.souzaf@hotmail.com>
2023-05-05 20:30:32 -0300

    Adding styles navbar

--- frontend-ecommerce/src/app/app.module.ts
+++ frontend-ecommerce/src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './interfaces/navbar/navbar.component';
-import { OlaComponent } from './interfaces/ola/ola.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    OlaComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
