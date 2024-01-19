1abe137afd825400c0831543b50dbbcdf750c46b
Caique Souza Fonseca <c.souzaf@hotmail.com>
2024-01-18 23:12:45 -0300

    organization

--- src/main/java/api/ecommerce/br/apiecommerce/service/ProductsService.java
+++ src/main/java/api/ecommerce/br/apiecommerce/service/ProductsService.java
@@ -1,19 +1,15 @@
 package api.ecommerce.br.apiecommerce.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import api.ecommerce.br.apiecommerce.config.auth.AuthenticationRequest;
+import api.ecommerce.br.apiecommerce.exception.ProductsException;
 import api.ecommerce.br.apiecommerce.exception.ResourceNotFoundException;
 import api.ecommerce.br.apiecommerce.model.Products;
+import api.ecommerce.br.apiecommerce.model.UserModel;
 import api.ecommerce.br.apiecommerce.repository.ProductsRepository;
-
+import api.ecommerce.br.apiecommerce.repository.UserRepository;
 import lombok.AllArgsConstructor;
 
 @Service
@@ -30,19 +26,7 @@ public class ProductsService {
     public Iterable<Products> listProducts(){
         return this.productsRepository.findAll();
     }
-
-    public Authentication authenticate(AuthenticationRequest request) {
-       
-        Authentication authentication = new UsernamePasswordAuthenticationToken(
-            request.getEmail(),
-            request.getPassword()
-        );
-        
-        // Chame o AuthenticationManager para autenticar
-        return authenticationManager.authenticate(authentication);
-    }
-    
-   
+  
 
     public Products createProducts(Products products){
     
