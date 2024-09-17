0194bf237a2236785897467459d9209ca1df462d
Caique Souza Fonseca <c.souzaf@hotmail.com>
2024-09-16 22:33:31 -0300

    atualizações

--- src/main/java/api/ecommerce/br/apiecommerce/config/SecurityUserConfig.java
+++ src/main/java/api/ecommerce/br/apiecommerce/config/SecurityUserConfig.java
@@ -21,6 +21,7 @@ public class SecurityUserConfig {
     private final JwTAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
