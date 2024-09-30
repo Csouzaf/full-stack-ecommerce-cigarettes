db5d270b93f206c2b3a259494997344fc227e09b
Caique Souza Fonseca <c.souzaf@hotmail.com>
2024-09-30 14:43:08 -0300

    Alterações pro eclipse

---
+++ frontend-ecommerce/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './interfaces/navbar/navbar.component';
+import { SidebarComponent } from './app/sidebar/sidebar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
